df8552d11ddd9265c0dcdb93285657a7c31bd72a
lcbfighting <312511278@qq.com>
2016-06-17 23:21:38 +0800

    引用V层，测试V层显示数据

--- application/index/controller/TeacherController.php
+++ application/index/controller/TeacherController.php
@@ -1,25 +1,36 @@
 <?php
 namespace app\index\controller;
+use think\Controller;//用于与V层进行数据传递
 use app\model\Teacher; //teacher表
 
 /**
  * 教师管理
  */
-class TeacherController
+class TeacherController extends Controller
 {
 	public function index()
 	{
 		$Teacher = new Teacher;
 		$teachers = $Teacher -> select();
+		
+		// 向V层传数据
+		$this->assign('teachers', $teachers);
+       
+        // 取回打包后的数据
+        $htmls = $this->fetch();
+
+        // 将数据返回给用户
+        return $htmls;
+
 		// var_dump($teachers);
 		// 获取第0个数据
-		$teacher = $teachers[0];
+		// $teacher = $teachers[0];
 
-		// 调用上述对象的getData()方法
-        var_dump($teacher -> getData());
-		echo $teacher -> getData('name');
-		var_dump($teacher -> getData('name'));
-		return $teacher -> getData('name');
+		// // 调用上述对象的getData()方法
+  //       var_dump($teacher -> getData());
+		// echo $teacher -> getData('name');
+		// var_dump($teacher -> getData('name'));
+		// return $teacher -> getData('name');
 		// return 'hello teacher';//测试类及方法的正确性
 		//获取教师表中所有数据
 		// $teachers = Db::name('teacher')->select();
---
+++ runtime/temp/4a5c83ae2183cb1c8050a5274934a515.php
@@ -0,0 +1,3 @@
+<?php if (!defined('THINK_PATH')) exit(); /*a:1:{s:88:"D:\xampp\htdocs\thinkphp5-samplecode\public/../application/index\view\teacher\index.html";i:1466176665;}*/ ?>
+I am V  nihao!
+<?php echo var_dump($teachers); ?>
