e055fc08520244c1d551a2e867246a10e0752073
lcbfighting <312511278@qq.com>
2016-06-18 00:09:14 +0800

    加入bootstrap样式

--- runtime/temp/4a5c83ae2183cb1c8050a5274934a515.php
+++ runtime/temp/4a5c83ae2183cb1c8050a5274934a515.php
@@ -1,4 +1,4 @@
-<?php if (!defined('THINK_PATH')) exit(); /*a:1:{s:88:"D:\xampp\htdocs\thinkphp5-samplecode\public/../application/index\view\teacher\index.html";i:1466179356;}*/ ?>
+<?php if (!defined('THINK_PATH')) exit(); /*a:1:{s:88:"D:\xampp\htdocs\thinkphp5-samplecode\public/../application/index\view\teacher\index.html";i:1466179645;}*/ ?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -6,25 +6,28 @@
 	<link rel="stylesheet" type="text/css" href="/thinkphp5-samplecode/public/static/bootstrap/css/bootstrap.min.css">
 </head>
 </head>
-<body>
-	<table>
-		<tr>
-			<th>序号</th>
-			<th>姓名</th>
-			<th>性别</th>
-			<th>用户名</th>
-			<th>邮箱</th>
-		</tr>
-		<?php if(is_array($teachers) || $teachers instanceof \think\Collection): $i = 0; $__LIST__ = $teachers;if( count($__LIST__)==0 ) : echo "" ;else: foreach($__LIST__ as $key=>$teacher): $mod = ($i % 2 );++$i;?>
-		<tr>
-			<td><?php echo $key; ?></td>
-			<td><?php echo $teacher->getData("name"); ?></td>
-			<td><?php if($teacher->getData("sex") == '0'): ?>男<?php else: ?>女<?php endif; ?></td>
-			<td><?php echo $teacher->getData("username"); ?></td>
-			<td><?php echo $teacher->getData("email"); ?></td>
-		</tr>
-		<?php endforeach; endif; else: echo "" ;endif; ?>
-	</table>
-	
+<body class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <table class="table table-hover table-bordered">
+			<tr class="info">
+				<th>序号</th>
+				<th>姓名</th>
+				<th>性别</th>
+				<th>用户名</th>
+				<th>邮箱</th>
+			</tr>
+			<?php if(is_array($teachers) || $teachers instanceof \think\Collection): $i = 0; $__LIST__ = $teachers;if( count($__LIST__)==0 ) : echo "" ;else: foreach($__LIST__ as $key=>$teacher): $mod = ($i % 2 );++$i;?>
+			<tr>
+				<td><?php echo $key; ?></td>
+				<td><?php echo $teacher->getData("name"); ?></td>
+				<td><?php if($teacher->getData("sex") == '0'): ?>男<?php else: ?>女<?php endif; ?></td>
+				<td><?php echo $teacher->getData("username"); ?></td>
+				<td><?php echo $teacher->getData("email"); ?></td>
+			</tr>
+			<?php endforeach; endif; else: echo "" ;endif; ?>
+		 </table>
+        </div>
+    </div>
 </body>
 </html>
